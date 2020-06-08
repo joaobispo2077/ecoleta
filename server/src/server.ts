48e6ae8cd22a2ce7c660c412e3492a7573ce35fe
João Bispo <vitor.vi10@hotmail.com>
2020-06-07 23:57:55 -0300

    :seedling: base

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import express, { response } from 'express';
+import path from 'path';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json())
+
+
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.listen(3333);
+
+// usage emoji: 🌱   🌲   🌳  🍏  💚
